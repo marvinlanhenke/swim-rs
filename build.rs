6c03761ac2db16a81d84b9cc7a2a3fdd21da5175
marvinlanhenke <lanhenke@googlemail.com>
2024-09-23 12:10:52 +0200

    chore: change compile_protos cfg + add pb mod

--- build.rs
+++ build.rs
@@ -4,9 +4,7 @@ fn main() -> Result<()> {
     let includes = ["./proto/"];
     let protos = ["./proto/swim.proto"];
 
-    prost_build::Config::new()
-        .type_attribute("NodeId", "#[derive(Eq, Hash)]")
-        .compile_protos(&protos, &includes)?;
+    prost_build::compile_protos(&protos, &includes)?;
 
     Ok(())
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,5 @@
 pub mod error;
+
+pub mod pb {
+    include!(concat!(env!("OUT_DIR"), "/swim.rs"));
+}
