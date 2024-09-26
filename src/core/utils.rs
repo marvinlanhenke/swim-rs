15106459781b97c6aa291468e94bc3aec1155671
marvinlanhenke <lanhenke@googlemail.com>
2024-09-26 14:01:41 +0200

    chore: add test 'utils send_action'

--- src/core/utils.rs
+++ src/core/utils.rs
@@ -15,3 +15,34 @@ pub async fn send_action<T: TransportLayer>(
 
     Ok(())
 }
+
+#[cfg(test)]
+mod tests {
+    use crate::{
+        core::utils::send_action,
+        pb::{
+            swim_message::{Action, Ping},
+            SwimMessage,
+        },
+        test_utils::mocks::MockUdpSocket,
+    };
+
+    #[tokio::test]
+    async fn test_utils_send_action() {
+        let socket = MockUdpSocket::new();
+        let action = Action::Ping(Ping {
+            from: "NODE_A".to_string(),
+            requested_by: "".to_string(),
+            gossip: None,
+        });
+
+        send_action(&socket, &action, "NODE_B").await.unwrap();
+
+        let result = &socket.transmitted().await[0];
+        let expected = SwimMessage {
+            action: Some(action),
+        };
+
+        assert_eq!(result, &expected);
+    }
+}
