7ad8f09cae564e88b10d605b9056661e69caa5e1
marvinlanhenke <lanhenke@googlemail.com>
2024-09-28 16:21:01 +0200

    chore: move init_tracing

--- src/api/mod.rs
+++ src/api/mod.rs
@@ -1,2 +1,17 @@
+use lazy_static::lazy_static;
+use tracing_subscriber::EnvFilter;
+
 pub mod config;
 pub mod swim;
+
+lazy_static! {
+    static ref TRACING: () = {
+        tracing_subscriber::fmt()
+            .with_env_filter(EnvFilter::from_default_env())
+            .init();
+    };
+}
+
+fn init_tracing() {
+    lazy_static::initialize(&TRACING);
+}
