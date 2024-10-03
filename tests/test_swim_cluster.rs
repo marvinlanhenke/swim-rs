c8525a2210b3be35a4df46bde3e4dd18461b8434
marvinlanhenke <lanhenke@googlemail.com>
2024-10-03 05:38:41 +0200

    chore: add test node_recover

--- tests/test_swim_cluster.rs
+++ tests/test_swim_cluster.rs
@@ -31,17 +31,59 @@ macro_rules! assert_event {
     };
 }
 
-async fn create_single_node(ms: u64, known_peers: &[&str]) -> SwimCluster {
+async fn create_single_node_with_addr(ms: u64, known_peers: &[&str], addr: &str) -> SwimCluster {
     let duration = Duration::from_millis(ms);
     let config = SwimConfig::builder()
         .with_ping_interval(duration)
         .with_ping_timeout(duration)
         .with_ping_req_timeout(duration)
-        .with_suspect_timeout(duration)
+        .with_suspect_timeout(duration * 10)
         .with_known_peers(known_peers)
         .build();
-    let addr = "127.0.0.1:0";
     SwimCluster::try_new(addr, config).await.unwrap()
+}
+
+async fn create_single_node(ms: u64, known_peers: &[&str]) -> SwimCluster {
+    let addr = "127.0.0.1:0";
+    create_single_node_with_addr(ms, known_peers, addr).await
+}
+
+#[tokio::test]
+async fn test_swim_cluster_node_recover_event() {
+    let ms = 10;
+    let node1 = create_single_node(ms, &[]).await;
+    let node2 = create_single_node(ms, &[node1.addr()]).await;
+    let node3 = create_single_node(ms, &[node1.addr()]).await;
+
+    node1.run().await;
+    node2.run().await;
+    let handles = node3.run().await;
+
+    let mut rx1 = node1.subscribe();
+    let mut rx2 = node2.subscribe();
+    let mut rx3 = node3.subscribe();
+
+    assert_event!(Event::NodeJoined, rx1, 1000, |_| true);
+    assert_event!(Event::NodeJoined, rx2, 1000, |_| true);
+    assert_event!(Event::NodeJoined, rx3, 1000, |_| true);
+
+    tracing::info!("[{}] is shutting down...", node3.addr());
+    handles.0.abort();
+    handles.1.abort();
+    let node3_addr = node3.addr().to_string();
+    drop(node3);
+    drop(rx3);
+
+    assert_event!(Event::NodeSuspected, rx1, 1000, |_| true);
+    assert_event!(Event::NodeSuspected, rx2, 1000, |_| true);
+
+    let node3 = create_single_node_with_addr(ms, &[node1.addr()], &node3_addr).await;
+    node3.run().await;
+    let mut rx3 = node3.subscribe();
+
+    assert_event!(Event::NodeRecovered, rx1, 1000, |_| true);
+    assert_event!(Event::NodeRecovered, rx2, 1000, |_| true);
+    assert_event!(Event::NodeRecovered, rx3, 1000, |_| true);
 }
 
 #[tokio::test]
