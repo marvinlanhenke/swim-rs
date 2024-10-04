a084285eb75dec294c7f8cf31fdaddb6f5a7dd64
marvinlanhenke <lanhenke@googlemail.com>
2024-10-04 13:05:54 +0200

    chore: add example to docs

--- src/lib.rs
+++ src/lib.rs
@@ -43,6 +43,54 @@
 //!     Ok(())
 //! }
 //! ```
+//! ## Event Subscription Example
+//!
+//! The following example demonstrates how to subscribe to events emitted by nodes in the SWIM cluster.
+//!
+//! ```rust,no_run
+//! use std::time::Duration;
+//!
+//! use swim_rs::{
+//!     api::{config::SwimConfig, swim::SwimCluster},
+//!     Event, Result,
+//! };
+//!
+//! #[tokio::main]
+//! async fn main() -> Result<()> {
+//!     // Creates two nodes in the same cluster
+//!     let node1 = SwimCluster::try_new("127.0.0.1:8080", SwimConfig::new()).await?;
+//!     let node2 = SwimCluster::try_new(
+//!         "127.0.0.1:8081",
+//!         SwimConfig::builder()
+//!             .with_known_peers(["127.0.0.1:8080"])
+//!             .build(),
+//!     )
+//!     .await?;
+//!
+//!     // Run the SWIM protocol in the background
+//!     node1.run().await;
+//!     node2.run().await;
+//!
+//!     // Subscribe and receive events from node1
+//!     let mut rx1 = node1.subscribe();
+//!
+//!     // Handle events accordingly
+//!     while let Ok(event) = rx1.recv().await {
+//!        match event {
+//!            Event::NodeJoined(e) => tracing::info!("[{}] handle {:#?}", node1.addr(), e),
+//!            Event::NodeSuspected(e) => tracing::info!("[{}] handle {:#?}", node1.addr(), e),
+//!            Event::NodeRecovered(e) => tracing::info!("[{}] handle {:#?}", node1.addr(), e),
+//!            Event::NodeDeceased(e) => tracing::info!("[{}] handle {:#?}", node1.addr(), e),
+//!        }
+//!     }
+//!
+//!     // Simulate a long-running process or service
+//!     tokio::time::sleep(Duration::from_secs(12)).await;
+//!
+//!     Ok(())
+//! }
+//! ```
+//!
 //! ## Learn More
 //!
 //! - [SWIM Protocol Paper](https://www.cs.cornell.edu/projects/Quicksilver/public_pdfs/SWIM.pdf)
