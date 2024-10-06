f8070139d8122bee292ee0faeab89f96a1b1b3bc
marvinlanhenke <lanhenke@googlemail.com>
2024-10-06 12:50:20 +0200

    chore: remove lint

--- src/lib.rs
+++ src/lib.rs
@@ -95,9 +95,6 @@
 //!
 //! - [SWIM Protocol Paper](https://www.cs.cornell.edu/projects/Quicksilver/public_pdfs/SWIM.pdf)
 //! - [GitHub Repository](https://github.com/marvinlanhenke/swim-rs)
-
-#![deny(missing_docs)]
-
 pub mod api;
 
 mod core;
