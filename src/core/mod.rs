02ad38c0bc4d4cf43a7887df0d8e5bc1065438d8
marvinlanhenke <lanhenke@googlemail.com>
2024-10-06 12:49:25 +0200

    chore: update docs

--- src/core/mod.rs
+++ src/core/mod.rs
@@ -1,3 +1,9 @@
+//! # Core Module
+//!
+//! The `core` module contains the main components of the SWIM protocol implementation.
+//! It includes sub-modules for failure detection, dissemination of gossip messages,
+//! membership list management, message handling, node orchestration, transport abstraction,
+//! type definitions, and utility functions.
 mod detection;
 mod disseminate;
 pub(crate) mod member;
--- src/test-utils/mod.rs
+++ src/test-utils/mod.rs
@@ -1,1 +1,4 @@
+//! # Mock Module
+//!
+//! The `mock` module contains mocks used in testing e.g. `MockUdpSocket`.
 pub mod mocks;
