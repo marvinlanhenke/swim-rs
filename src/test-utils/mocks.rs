593ea5d59cfdadbd40f73e87a795ff1e142f6b8f
marvinlanhenke <lanhenke@googlemail.com>
2024-10-06 12:47:08 +0200

    chore: update docs

--- src/test-utils/mocks.rs
+++ src/test-utils/mocks.rs
@@ -1,3 +1,8 @@
+//! # Mock UDP Socket Module
+//!
+//! This module provides a mock implementation of the `TransportLayer` trait for testing purposes.
+//! The `MockUdpSocket` struct simulates a UDP socket by storing transmitted and received messages
+//! in memory, allowing tests to verify the behavior of components that rely on network communication.
 use std::sync::Arc;
 
 use async_trait::async_trait;
@@ -8,32 +13,48 @@ use tokio::sync::Mutex;
 use crate::error::Result;
 use crate::{core::transport::TransportLayer, pb::SwimMessage};
 
+/// A mock implementation of a UDP socket for testing purposes.
+///
+/// The `MockUdpSocket` struct simulates sending and receiving UDP messages by storing them in
+/// memory. It implements the `TransportLayer` trait, allowing it to be used in place of a real
+/// UDP socket during testing.
 #[derive(Clone, Debug, Default)]
 pub(crate) struct MockUdpSocket {
+    /// A list of transmitted messages stored in an `Arc<Mutex<Vec<SwimMessage>>>`.
     transmitted: Arc<Mutex<Vec<SwimMessage>>>,
+    /// A list of received messages stored in an `Arc<Mutex<Vec<SwimMessage>>>`.
     received: Arc<Mutex<Vec<SwimMessage>>>,
 }
 
 impl MockUdpSocket {
+    /// Creates a new `MockUdpSocket` instance.
+    ///
+    /// # Returns
+    ///
+    /// A new `MockUdpSocket` with empty transmitted and received message lists.
     pub(crate) fn new() -> Self {
         Self::default()
     }
 
+    /// Retrieves the list of transmitted messages.
     pub(crate) async fn transmitted(&self) -> Vec<SwimMessage> {
         let tx = self.transmitted.lock().await;
         (*tx).clone()
     }
 
+    /// Adds a message to the list of transmitted messages.
     pub(crate) async fn add_transmitted(&self, message: SwimMessage) {
         let mut tx = self.transmitted.lock().await;
         tx.push(message);
     }
 
+    /// Retrieves the list of received messages.
     pub(crate) async fn received(&self) -> Vec<SwimMessage> {
         let rx = self.received.lock().await;
         (*rx).clone()
     }
 
+    /// Adds a message to the list of received messages.
     pub(crate) async fn add_received(&self, message: SwimMessage) {
         let mut rx = self.received.lock().await;
         rx.push(message);
